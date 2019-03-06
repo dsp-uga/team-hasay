08e93ad67ed71a6174dd526f7a01932998d02dba
Dhaval Bhanderi <44267526+dvlbhanderi@users.noreply.github.com>
2019-03-05 23:51:33 -0500

    Adding Class_convert, accuracy method

--- MRF.py
+++ MRF.py
@@ -72,3 +72,148 @@ def initial_energy_function(initial_w, pixels, betha, cls_info, neighbors_indice
                     energy += betha * differnce(w[i][j], w[a][b])
     return energy
 
+def exponential_schedule(step_number, current_t, initial_temp,  constant=0.99):
+    return current_t*constant
+
+def logarithmical_multiplicative_cooling_schedule(step_number, current_t, initial_temp, constant=1.0):
+    return initial_temp / (1 + constant * np.log(1+step_number))
+
+def linear_multiplicative_cooling_schedule(step_number, current_t, initial_temp, constant=1.0):
+    return initial_temp / (1 + constant * step_number)
+
+def delta_enegry(w, index, betha, new_value, neighbors_indices, pixels, cls_info):
+    initial_energy = 0 
+    (i,j) = index
+    rows = len(w)
+    cols = len(w[0])
+    mean = cls_info[int(w[i][j])][1]
+    var =  cls_info[int(w[i][j])][2]
+    initial_energy += np.log(np.sqrt(2*np.pi*var)) 
+    initial_energy += ((pixels[i][j]-mean)**2)/(2*var)
+    for a,b in neighbors_indices:
+        a +=i
+        b +=j
+        if 0<=a<rows and 0<=b<cols:
+            initial_energy += betha * differnce(w[i][j], w[a][b])  
+    new_energy = 0
+    mean = cls_info[new_value][1]
+    var =  cls_info[new_value][2]
+    new_energy += np.log(np.sqrt(2*np.pi*var)) 
+    new_energy += ((pixels[i][j]-mean)**2)/(2*var)
+    #print("first enegry", new_energy)
+
+    for a,b in neighbors_indices:
+        a +=i
+        b +=j
+        if 0<=a<rows and 0<=b<cols:
+            new_energy += betha * differnce(new_value, w[a][b])
+    #print("last energy", new_energy)
+    return new_energy - initial_energy
+
+def simulated_annealing(init_w, class_labels, temprature_function,
+                        pixels, betha, cls_info, neighbors_indices, max_iteration=10000,
+                        initial_temp = 1000, known_index=[], correct_arr = [], temprature_function_constant=None ):
+    partial_prediction=False
+    if (len(known_index)>0 and len(correct_arr)>0):
+        partial_prediction=True
+
+    w = np.array(init_w)
+    changed_array = np.zeros((len(w), len(w[0])))
+    iteration =0
+    x = len(w)
+    y = len(w[0])
+    current_energy = initial_energy_function(w, pixels, betha, cls_info, neighbors_indices)
+    current_tmp = initial_temp
+    while (iteration<max_iteration):
+        if (partial_prediction):
+            is_found=False
+            while (is_found==False):
+                i = random.randint(0, x-1)
+                j = random.randint(0, y-1)
+                if (known_index[i][j]==0):
+                    is_found=True
+        else:
+            i = random.randint(0, x-1)
+            j = random.randint(0, y-1)
+
+        l = list(class_labels)
+        l.remove(w[i][j])
+        r = random.randint(0, len(l)-1)
+        new_value = l[r]
+        delta = delta_enegry(w, (i,j), betha, new_value, neighbors_indices, pixels, cls_info)
+
+        r = random.uniform(0, 1)
+        if (delta<=0):
+            w[i][j]=new_value
+            current_energy+=delta
+            changed_array[i][j]+=1
+            #print("Changed to better")
+        else:
+            try:
+                if (-delta / current_tmp < -600):
+                    k=0
+                else:
+                    k = np.exp(-delta / current_tmp)
+            except:
+                k=0
+            if r < k:
+                #print("Changed to worst")
+                w[i][j] = new_value
+                current_energy += delta
+                changed_array[i][j] += 1
+        if (temprature_function_constant!=None):
+            current_tmp = temprature_function(iteration, current_tmp, initial_temp, constant =temprature_function_constant)
+        else:
+            current_tmp = temprature_function(iteration, current_tmp, initial_temp)
+        iteration+=1
+    return w, changed_array
+
+def convert_to_class_labels(arr, inverse_array={0:0, 127:1, 255:2}):
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr[0])):
+            arr[i][j] = inverse_array[int(arr[i][j])]
+
+def get_accuracy(arr, labels):
+    correct_labels = 0
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr[0])):
+            if (labels[i][j]==int(arr[i][j]/127)):
+                correct_labels+=1
+    return correct_labels/(len(arr[0])*len(arr))
+
+def a_complete_set_for_part_2 (arr, class_info, max_iter=1000000,var = 10000,
+                               betha = 100,
+                               neighbor_indices = [[0,1],[0,-1],[1,0],[-1,0]],
+                               class_labels = [0,1,2], 
+                               class_color = [0,127,255], 
+                               schedule= exponential_schedule,
+                               temprature_function_constant=None):
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1,4)
+#     fig.suptitle('Comparision', fontsize=20)
+    
+
+    ax1.set_title("first image")
+    ax1.imshow(arr, cmap='gray')
+    rows = len(arr)
+    cols = len(arr[0])
+    cls_info = class_info
+    initial_arr = naive_bayes_predict(arr, cls_info)
+    ax2.set_title('Naive Bayes image')
+    ax2.imshow(initial_arr, cmap='gray')
+
+    convert_to_class_labels(initial_arr)
+    
+    w, test_array = simulated_annealing(initial_arr, class_labels, schedule,
+                                        arr, betha, cls_info, neighbor_indices, max_iteration=max_iter)
+  
+    for i in range (0, len(w)):
+        for j in range(0, len(w[0])):
+            w[i][j] = class_color[int (w[i][j])]
+
+    ax3.set_title('CRF image')
+    ax3.imshow(w, cmap='gray')
+    plt.rcParams["figure.figsize"] = (20,3)
+    ax4.set_title('differ image')
+    ax4.imshow(test_array, cmap='gray') 
+    plt.show()
